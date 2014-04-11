84dbc018bf5d8f03f0e8075b25b5fb732b87e25d
Ryan Volz <ryan.volz@gmail.com>
2014-04-11 10:59:52 -0700

    Change license to BSD 3-clause. Although I believe this software should remain free and that modifications should also be open source, I trust social pressure to ensure that remains true. Most of the Python science software is BSD-licensed, so it is hoped that this switch will allow easier adoption in the future.

--- setup.py
+++ setup.py
@@ -1,21 +1,11 @@
-#!/usr/bin/env python
-
-# Copyright 2013 Ryan Volz
-
-# This file is part of radarmodel.
-
-# Radarmodel is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# Radarmodel is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with radarmodel.  If not, see <http://www.gnu.org/licenses/>.
+#-----------------------------------------------------------------------------
+# Copyright (c) 2014, Ryan Volz
+# All rights reserved.
+#
+# Distributed under the terms of the BSD 3-Clause ("BSD New") license.
+#
+# The full license is in the LICENSE file, distributed with this software.
+#-----------------------------------------------------------------------------
 
 import os
 import sys
