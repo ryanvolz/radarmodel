90878f63bac63631cc46155c35069fcf5818850d
Ryan Volz <ryan.volz@gmail.com>
2015-06-16 13:10:36 -0400

    Update _version.py.

--- radarmodel/_version.py
+++ radarmodel/_version.py
@@ -35,7 +35,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = ""
+    cfg.style = "pep440"
     cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "radarmodel-"
     cfg.versionfile_source = "radarmodel/_version.py"
